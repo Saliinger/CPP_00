e9e634da30bfcac8364f6706655f4738d8851c8d
Saliinger <88099624+Saliinger@users.noreply.github.com>
2024-08-31 18:23:20 +0200

    done ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: anoukan <anoukan@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/31 17:57:18 by anoukan           #+#    #+#             */
-/*   Updated: 2024/08/31 18:09:59 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/08/31 18:20:32 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,24 @@
 
 int	main(int ac, char **av)
 {
+	int	i = 0;
+	int	j = 1;
+
 	if (ac == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 		return (0);
 	}
+	while (av[j])
+	{
+		i = 0;
+		while (av[j][i])
+		{
+			std::cout << (char)toupper(av[j][i]);
+			i++;
+		}
+		j++;
+	}
+	std::cout << std::endl;
 	return (0);
 }
