2c0f87de02671b7ee0e3a3adc80b08a17b0f01f7
Saliinger <88099624+Saliinger@users.noreply.github.com>
2024-08-31 18:17:52 +0200

    done Makefile

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,15 +6,19 @@
 /*   By: anoukan <anoukan@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/31 17:57:18 by anoukan           #+#    #+#             */
-/*   Updated: 2024/08/31 18:00:22 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/08/31 18:09:59 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <cstring>
+
 int	main(int ac, char **av)
 {
-	if (ac == 2)
+	if (ac == 1)
 	{
-
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		return (0);
 	}
 	return (0);
 }
