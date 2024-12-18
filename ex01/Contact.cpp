f9bfe4f75d2d1c82c1513f8c120dc604357289ca
Saliinger <alexisnoukan@gmail.com>
2024-12-18 16:08:43 +0100

    add set contact methode

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -1,3 +1,18 @@
 #include "Contact.hpp"
 
+Contact::Contact(){
+
+}
+
+Contact::~Contact(){
+
+}
+
+void Contact::setContact(std::string name, std::string lastName, std::string nickname,std::string phone, std::string _darkestSecret){
+  this->_name = name;
+  this->_lastName = lastName;
+  this->_nickname = nickname;
+  this->_phone = phone;
+  this->_darkestSecret = _darkestSecret;
+}
 
--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -1,7 +1,3 @@
-//
-// Created by anoukan on 18/12/24.
-//
-
 #ifndef CONTACT_HPP
 #define CONTACT_HPP
 
@@ -9,11 +5,15 @@
 
 class Contact {
   private:
-    std::string name;
-    std::string lastName;
-    std::string nickname;
-    std::string phone;
-    std::string darkestSecret;
-}
+    std::string _name;
+    std::string _lastName;
+    std::string _nickname;
+    std::string _phone;
+    std::string _darkestSecret;
+    public:
+      Contact();
+      void setContact(std::string name, std::string lastName, std::string nickname,std::string phone, std::string _darkestSecret);
+      ~Contact();
+};
 
 #endif //CONTACT_HPP
